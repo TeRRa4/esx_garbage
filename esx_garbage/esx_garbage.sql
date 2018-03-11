d19b5d6f565c26343029ae5d3f7982377f5d4871
scorpio686 <31187145+scorpio686@users.noreply.github.com>
2018-03-11 22:34:35 +0100

    Update esx_garbage.sql

--- esx_garbage/esx_garbage.sql
+++ esx_garbage/esx_garbage.sql
@@ -1,8 +1,8 @@
 INSERT INTO `jobs` (`id`, `name`, `label`, `whitelisted`) VALUES
-(20, 'eboueur', 'Little Pricks', 0);
+(NULL, 'eboueur', 'Little Pricks', 0);
 
 INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-(50, 'eboueur', 0, 'interim', 'Intérimaire', 250, '{}', '{}');
+(NULL, 'eboueur', 0, 'interim', 'Intérimaire', 250, '{}', '{}');
 
 INSERT INTO `items` (`id`, `name`, `label`, `limit`, `height`, `rare`, `can_remove`) VALUES
-(2, 'poubelle', 'Poubelle', 100, 500, 0, 1);
+(NULL, 'poubelle', 'Poubelle', 100, 500, 0, 1);
