adf5c95cff064dee0a116a91b1bc42dc58995eeb
TeRRa4 <bevan.edwards93@gmail.com>
2018-08-22 12:35:33 +0100

    Translated to English

--- esx_garbage/esx_garbage.sql
+++ esx_garbage/esx_garbage.sql
@@ -2,7 +2,7 @@ INSERT INTO `jobs` (`id`, `name`, `label`, `whitelisted`) VALUES
 (NULL, 'eboueur', 'Little Pricks', 0);
 
 INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-(NULL, 'eboueur', 0, 'interim', 'Intérimaire', 250, '{}', '{}');
+(NULL, 'eboueur', 0, 'interim', 'Employee', 200, '{}', '{}');
 
 INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-(NULL, 'poubelle', 'Poubelle', -1, 0, 1);
+(NULL, 'poubelle', 'Trash', -1, 0, 1);
