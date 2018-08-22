0c1d9fa13f31b8e1550ec6ca2719ab6da932409c
TeRRa4 <bevan.edwards93@gmail.com>
2018-08-22 12:39:09 +0100

    Updated to match standard SQL layout

--- esx_garbage/esx_garbage.sql
+++ esx_garbage/esx_garbage.sql
@@ -1,8 +1,8 @@
-INSERT INTO `jobs` (`id`, `name`, `label`, `whitelisted`) VALUES
-(NULL, 'eboueur', 'Little Pricks', 0);
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('eboueur', 'Little Pricks', 0);
 
 INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
 (NULL, 'eboueur', 0, 'interim', 'Employee', 200, '{}', '{}');
 
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-(NULL, 'poubelle', 'Trash', -1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('poubelle', 'Trash', -1, 0, 1);
